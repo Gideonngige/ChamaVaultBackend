338fd50f07c927ce28d3825af6b67aa7c6323588
Gideonngige <ushindigideon01@gmail.com>
2025-05-23 09:06:47 +0300

    removed unnecessary apis

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 #'django-insecure-warma_a=ah#lg$($qcbr_$f_80ij@42e&ph^u-5bv$&u@5q&!l'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = 'django-insecure-warma_a=ah#lg$($qcbr_$f_80ij@42e&ph^u-5bv$&u@5q&!l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -182,47 +182,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#send email
-#email backend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ushindigideon01@gmail.com'
-EMAIL_HOST_PASSWORD = 'rrst fhrj pojn agkt'
-
-
-#daraja api
-# The Mpesa environment to use
-# Possible values: sandbox, production
-MPESA_ENVIRONMENT = 'sandbox'
-# Credentials for the daraja app
-MPESA_CONSUMER_KEY = '17tijAWZQBWLRIFFuJrDGBfl1zalwr00g6wEE20cGdeHvw7l'
-MPESA_CONSUMER_SECRET = 'iX29aYc7ujvLlXssKhvG2ilFzS7Bpoa5dU9SIGoPUDrdkLWwKQD1rUEOhW7BRQ3e'
-#Shortcode to use for transactions. For sandbox use the Shortcode 1
-#provided on test credentials page
-MPESA_SHORTCODE = '1'
-# Shortcode to use for Lipa na MPESA Online (MPESA Express) transactions
-# This is only used on sandbox, do not set this variable in production
-# For sandbox use the Lipa na MPESA Online Shorcode provided on test
-#credentials page
-MPESA_EXPRESS_SHORTCODE = '174379'
-# Type of shortcode
-# Possible values:
-# - paybill (For Paybill)
-# - till_number (For Buy Goods Till Number)
-MPESA_SHORTCODE_TYPE = '174379'
-# Lipa na MPESA Online passkey
-# Sandbox passkey is available on test credentials page
-# Production passkey is sent via email once you go live
-MPESA_PASSKEY = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
-# Username for initiator (to be used in B2C, B2B, AccountBalance and
-#TransactionStatusQuery Transactions)
-MPESA_INITIATOR_USERNAME = 'testapi'
-# Plaintext password for initiator (to be used in B2C, B2B, AccountBalance
-#and TransactionStatusQuery Transactions)
-MPESA_INITIATOR_SECURITY_CREDENTIAL = 'Skb4kUuWtvsO//466Z4Jafbr3/9zZ8B7IxW4nutAPmOAcvEU4xFOVy0OaSVIJhXUFc+TIKatiXWnlkVWVefJK5kxLnq0FTQMzBGam8Yle4y/bNLj5b+jgieqpUZPfYceqktqPEjkH9K+XOhT7VIABCc/klFAXOvapx/yVesOkCgkVKbe4/vpBlA74zJ/9cH+KQyk32l+asLtgPr8qiYq3nT3HDVeYof6hQT/vQXUtd+RVHGuU8glfFj55cC2nv4/eQEKqkeHVgOardY8GXqW65vtNQSVqNec5ETLxsEqy+zbNoBZEt6uTtCNwxl2c9rcHUYKLHigVT+YUl4awnN5eQ=='
 
 
 import cloudinary
